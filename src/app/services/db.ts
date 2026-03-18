66625ca71c7cd768e1450d1774de6e1b5a7a3332
Akseli Nurmio <akseli@nurmio.fi>
2026-03-18 15:35:14 +0200

    Fix db connection

--- src/app/services/db.ts
+++ src/app/services/db.ts
@@ -2,7 +2,8 @@ import { PrismaClient } from "generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
-  connectionString: process.env.POSTGRES_PRISMA_URL!,
+  connectionString: process.env.POSTGRES_URL,
+  ssl: true,
 });
 
 export const prisma = new PrismaClient({ adapter });
